bf8d3c532900be6cf2a1fa0b383b645a82a8c818
shaaibu <shaaibusuleiman@gamil.com>
2023-04-28 10:38:39 +0100

    Create solver and refactor test file accordingly

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,12 +1,12 @@
-class Factorial 
-  def compute_factorial(n)
+class Solver
+  def factorial(n)
     result = 1
     if n == 0
       return 1
     elsif n < 0
       return 'Only positive numbers are allowed'
     else
-      result = compute_factorial(n - 1) * n
+      result = factorial(n - 1) * n
       n -= 1
       result
     end
--- spec/factorial_test_spec.rb
+++ spec/factorial_test_spec.rb
@@ -1,22 +1,22 @@
-require_relative '../classes/factorial'
+require_relative '../classes/solver'
 
-describe Factorial do
+describe Solver do
   
   context 'Test for factorial of any number' do
     before(:context) do
-      @factorial = Factorial.new
+      @factorial = Solver.new
     end
 
     it 'Should return one for N = 0' do
-      expect(@factorial.compute_factorial(0)).to eq 1
+      expect(@factorial.factorial(0)).to eq 1
     end
 
     it 'Should return an error for negative numbers' do
-      expect(@factorial.compute_factorial(-2)).to eq 'Only positive numbers are allowed'
+      expect(@factorial.factorial(-2)).to eq 'Only positive numbers are allowed'
     end
 
     it 'Should return factorial of positive numbers' do
-      expect(@factorial.compute_factorial(5)).to eq 120
+      expect(@factorial.factorial(5)).to eq 120
     end
   end
 end
